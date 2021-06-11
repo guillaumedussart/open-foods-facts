3e1459ed23a37e2f43d2201ca6527b8d3fbe675a
guillaume dussart <guillaume@localdev.local>
2021-06-11 18:56:49 +0200

    hydrate categories class

--- src/main/java/org/fantasticfour/csv/ConfigCSV.java
+++ src/main/java/org/fantasticfour/csv/ConfigCSV.java
@@ -1,50 +1,59 @@
 package org.fantasticfour.csv;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
-
-public class ConfigCSV  {
-
-
-
+public class ConfigCSV {
 
 
     public static void main(String[] args) throws IOException {
 
 
-
-         Path paths = Paths.get("C:/Users/dimit/OneDrive/Bureau/projet wika/open-food-facts.csv");
-
-
-      List<String> lines = Files.readAllLines(paths,StandardCharsets.UTF_8);
+        Path paths = Paths.get("/workspace/www/traitement-fichier-jpa-off/src/main/resources/open-food-facts.csv");
 
 
+        List<String> lines = Files.readAllLines(paths, StandardCharsets.UTF_8);
 
+        Iterator<String> fileIte = lines.iterator();
+        HashSet<String> deleteSame = new HashSet<>();
 
-
-        ArrayList<String>infotab= new ArrayList<>();
-
-        for (int i = 1; i <= lines.size() -1; i++){
-            String ligne = lines.get(i);
-            String lignes= ligne.trim().replaceAll("|",";");
-            String[] morceau = lignes.split(";");
-
-
-
-            System.out.println(morceau[2]);
-            //String popTotal = morceau[9].trim().replaceAll(" ", "");
-            //int popTotales = Integer.parseInt(popTotal);
-
+        List<Category> allCategories = new ArrayList<>();
+        for (int i = 1; i < lines.size(); i++) {
+            String line = lines.get(i);
+            String[] part = line.split("\\|");
+            String categorie = part[0];
+            if (deleteSame.add(categorie)) {
+                Category categories = new Category(categorie);
+                System.out.println(categories.getName());
+                allCategories.add(categories);
+            }
         }
+        System.out.println(allCategories);
+ /*       while (fileIte.hasNext()) {
+            String[] getIngredient = fileIte.next().split("\\|");
+            String ingredient = getIngredient[0];
+            String replaceEnderscore = ingredient.replace("_","");
+            //System.out.println(replaceEnderscore);
+            if (deleteSame.add(ingredient)) {
+                System.out.println(ingredient);
+            }
+            *//*List<String> blockI = Arrays.asList(replaceEnderscore.split(","));
+            System.out.println("---------------------------------");
+            System.out.println(getIngredient[0]);
+            System.out.println(getIngredient[1]);
+            System.out.println(getIngredient[2]);
+            System.out.println("________________________________");
+            for (int i =0;i<blockI.size();i++){
+                System.out.println(blockI.get(i));
+            }*//*
+        }
+*/
+
     }
-
-
 
 }
