5e8b96c4c6ddede7ffeae084e02efef379638ff8
Poyatos Rémi <poyatosremi@gmail.com>
2021-06-11 14:43:07 +0200

    IProductDAO

---
+++ src/main/java/org/fantasticfour/dal/IProductDAO.java
@@ -0,0 +1,5 @@
+package org.fantasticfour.dal;
+
+import org.fantasticfour.bo.Product;
+
+public interface IProductDAO extends IDAO<Product, Long>{}
---
+++ src/main/java/org/fantasticfour/dal/jpa/ProductJPADAO.java
@@ -0,0 +1,49 @@
+package org.fantasticfour.dal.jpa;
+
+import java.sql.SQLException;
+import java.util.Set;
+
+import org.fantasticfour.bo.Product;
+import org.fantasticfour.dal.IProductDAO;
+
+public class ProductJPADAO implements IProductDAO {
+
+	@Override
+	public void create(Product o) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Product findById(Long id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Product> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Product o) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Product o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Long id) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
