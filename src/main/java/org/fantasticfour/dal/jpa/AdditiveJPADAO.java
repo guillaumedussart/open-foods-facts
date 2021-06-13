f35404339f5d2d05d33975aa63162dc208236a69
Poyatos Rémi <poyatosremi@gmail.com>
2021-06-13 19:18:20 +0200

    ajout de la methode create additive

--- src/main/java/org/fantasticfour/dal/jpa/AdditiveJPADAO.java
+++ src/main/java/org/fantasticfour/dal/jpa/AdditiveJPADAO.java
@@ -9,17 +9,24 @@ import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 import org.fantasticfour.bo.Additive;
+import org.fantasticfour.bo.Category;
 import org.fantasticfour.dal.IAdditiveDAO;
 
-public class AdditiveJPADAO implements IAdditiveDAO{
+public class AdditiveJPADAO implements IAdditiveDAO {
 
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("open-food-facts");
 	private EntityManager em = emf.createEntityManager();
-	
+
 	@Override
 	public void create(Additive o) throws SQLException {
-		// TODO Auto-generated method stub
-		
+		em.getTransaction().begin();
+
+		Additive additive = new Additive(o.getName());
+
+		em.persist(additive);
+
+		em.getTransaction().commit();
+
 	}
 
 	@Override
@@ -33,33 +40,33 @@ public class AdditiveJPADAO implements IAdditiveDAO{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public Additive findByName(String string) throws SQLException {
 		TypedQuery<Additive> query = em.createQuery("SELECT name FROM categories WHERE name =:name ", Additive.class);
 		query.setParameter("name", string);
-		
+
 		Additive additive = query.getSingleResult();
-		
+
 		return additive;
-		
+
 	}
 
 	@Override
 	public void update(Additive o) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delete(Additive o) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Long id) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
